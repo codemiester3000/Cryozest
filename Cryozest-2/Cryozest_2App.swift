3cd4c6086387616c5029dd595ba10b6e9f5d1b4e
codemiester3000 <owenkamilkhoury@gmail.com>
2023-03-21 21:15:35 -0400

    Initial Commit

---
+++ Cryozest-2/Cryozest_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Cryozest_2App.swift
+//  Cryozest-2
+//
+//  Created by Owen Khoury on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Cryozest_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
