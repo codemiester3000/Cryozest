12bfdb0d4d7c2c481dc97374c68c24546f1c32bd
codemiester3000 <owenkamilkhoury@gmail.com>
2023-04-09 13:38:21 -0400

    update logbook view design

--- Cryozest-2/SessionRow.swift
+++ Cryozest-2/SessionRow.swift
@@ -5,31 +5,35 @@ struct SessionRow: View {
 
     var body: some View {
         HStack {
-            VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 8) {
                 Text(session.date ?? "")
-                    .font(.system(size: 18, design: .rounded))
+                    .font(.system(size: 24, weight: .bold))
                     .foregroundColor(.white)
                 
                 Text(session.therapyType ?? "")
-                    .font(.system(size: 14, design: .rounded))
+                    .font(.system(size: 18))
                     .foregroundColor(.white)
             }
 
             Spacer()
 
-            VStack(alignment: .trailing) {
+            VStack(alignment: .trailing, spacing: 8) {
                 Text("Duration: \(String(format: "%02d", Int(session.duration) / 60)):\(String(format: "%02d", Int(session.duration) % 60))")
-                    .font(.system(size: 14, design: .rounded))
+                    .font(.system(size: 18))
                     .foregroundColor(.white)
                 
                 Text("Temp: \(Int(session.temperature))°F")
-                    .font(.system(size: 14, design: .rounded))
+                    .font(.system(size: 18))
                     .foregroundColor(.white)
             }
         }
-        .padding(.vertical, 8)
-        .padding(.horizontal)
-        .background(.blue)
-        .cornerRadius(8)
+        .padding(.horizontal, 24)
+        .padding(.vertical, 16)
+        .background(Color.gray)
+        .overlay(
+            RoundedRectangle(cornerRadius: 16)
+                .stroke(Color.orange, lineWidth: 4)
+        )
+        .cornerRadius(16)
     }
 }
