0113aff4546cededf5c50182940742423bbc7093
robertamarin <127469693+robertamarin@users.noreply.github.com>
2023-05-25 22:52:11 -0400

    Update TherapySession.swift

--- Cryozest-2/TherapySession.swift
+++ Cryozest-2/TherapySession.swift
@@ -9,19 +9,22 @@ struct TherapySession: Codable, Identifiable {
     let temperature: Double
     let humidity: Int
     let therapyType: TherapyType
+    let bodyWeight: Double
     
-    init(date: String, duration: TimeInterval, temperature: Double, humidity: Int, therapyType: TherapyType) {
-        self.id = UUID()
-        self.date = date
-        self.duration = duration
-        self.temperature = temperature
-        self.humidity = humidity
-        self.therapyType = therapyType
+    init(date: String, duration: TimeInterval, temperature: Double, humidity: Int, therapyType: TherapyType, bodyWeight: Double) {
+            self.id = UUID()
+            self.date = date
+            self.duration = duration
+            self.temperature = temperature
+            self.humidity = humidity
+            self.therapyType = therapyType
+            self.bodyWeight = bodyWeight
+        }
+        
+        var formattedDuration: String {
+            let minutes = Int(duration) / 60
+            let seconds = Int(duration) % 60
+            return String(format: "%02d:%02d", minutes, seconds)
+        }
     }
-    
-    var formattedDuration: String {
-        let minutes = Int(duration) / 60
-        let seconds = Int(duration) % 60
-        return String(format: "%02d:%02d", minutes, seconds)
-    }
-}
+
