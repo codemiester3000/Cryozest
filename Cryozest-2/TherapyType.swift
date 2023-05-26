435aeef7e565952d966f14f73c4ef06ed37df19c
robertamarin <127469693+robertamarin@users.noreply.github.com>
2023-05-26 13:09:57 -0400

    Update TherapyType.swift

--- Cryozest-2/TherapyType.swift
+++ Cryozest-2/TherapyType.swift
@@ -7,4 +7,17 @@ enum TherapyType: String, Codable, Identifiable, CaseIterable {
     case coldShower = "Cold Shower"
     
     var id: String { self.rawValue }
+    
+    var icon: String {
+        switch self {
+        case .drySauna:
+            return "flame.fill" // Suggests heat
+        case .steamRoom:
+            return "smoke.fill" // Suggests steam
+        case .coldPlunge:
+            return "thermometer.snowflake" // Suggests cold water
+        case .coldShower:
+            return "drop.fill" // Suggests a shower
+        }
+    }
 }
