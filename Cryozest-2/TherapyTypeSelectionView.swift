6f8499af175f584bb85a8c93017f94110f82de91
codemiester3000 <owenkamilkhoury@gmail.com>
2023-06-25 01:57:12 -0400

    alerting on the therapy selection view

--- Cryozest-2/TherapyTypeSelectionView.swift
+++ Cryozest-2/TherapyTypeSelectionView.swift
@@ -5,73 +5,91 @@ struct TherapyTypeSelectionView: View {
     @Environment(\.presentationMode) var presentationMode
     @Environment(\.managedObjectContext) var managedObjectContext
     @State var selectedTypes: [TherapyType] = []
+    @State var showAlert = false
+    @State var alertTitle = ""
+    @State var alertMessage = ""
     
     @FetchRequest(
-            entity: SelectedTherapy.entity(),
-            sortDescriptors: []
-        ) private var selectedTherapies: FetchedResults<SelectedTherapy>
-
+        entity: SelectedTherapy.entity(),
+        sortDescriptors: []
+    ) private var selectedTherapies: FetchedResults<SelectedTherapy>
+    
     var body: some View {
-                ZStack {
-                    LinearGradient(
-                        gradient: Gradient(colors: [Color.gray, Color.gray.opacity(0.8)]),
-                        startPoint: .top,
-                        endPoint: .bottom
-                    )
-                    .edgesIgnoringSafeArea(.all)
-                    
-                    VStack {
-                        Text("Select up to 4 therapy types")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            //.padding()
-
-                        ScrollView {  // wrap ForEach with ScrollView
-                            ForEach(TherapyType.allCases, id: \.self) { therapyType in
-                                Button(action: {
-                                    if selectedTypes.contains(therapyType) {
-                                        selectedTypes.removeAll(where: { $0 == therapyType })
-                                    } else if selectedTypes.count < 4 {
-                                        selectedTypes.append(therapyType)
-                                    }
-                                }) {
-                                    HStack {
-                                        Image(systemName: therapyType.icon)
-                                            .foregroundColor(.white)
-                                        Text(therapyType.rawValue)
-                                            .foregroundColor(.white)
-                                        Spacer()
-                                        if selectedTypes.contains(therapyType) {
-                                            Image(systemName: "checkmark")
-                                                .foregroundColor(.white)
-                                        }
-                                    }
-                                    .padding()
-                                    .background(therapyType.color.opacity(0.8))
-                                    .cornerRadius(8)
-                                }
-                                .padding(.horizontal)
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [Color.gray, Color.gray.opacity(0.8)]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Select up to 4 therapy types")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding(.bottom, 8)
+                
+                ScrollView {
+                    ForEach(TherapyType.allCases, id: \.self) { therapyType in
+                        Button(action: {
+                            if selectedTypes.contains(therapyType) {
+                                selectedTypes.removeAll(where: { $0 == therapyType })
+                            } else if selectedTypes.count < 4 {
+                                selectedTypes.append(therapyType)
+                            } else {
+                                // user tried to select a 5th type
+                                alertTitle = "Too Many Types"
+                                alertMessage = "Please remove a type before adding another."
+                                showAlert = true
                             }
+                        }) {
+                            HStack {
+                                Image(systemName: therapyType.icon)
+                                    .foregroundColor(.white)
+                                Text(therapyType.rawValue)
+                                    .foregroundColor(.white)
+                                Spacer()
+                                if selectedTypes.contains(therapyType) {
+                                    Image(systemName: "checkmark")
+                                        .foregroundColor(.white)
+                                }
+                            }
+                            .padding()
+                            .background(therapyType.color.opacity(0.8))
+                            .cornerRadius(8)
                         }
-
-                        Spacer()
-                        Button("Done") {
-                            // Save the selected types and dismiss the view.
-                            saveSelectedTherapies(therapyTypes: selectedTypes, context: managedObjectContext)
-                            presentationMode.wrappedValue.dismiss()
-                        }
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
+                        //.padding(.horizontal)
                     }
-                    .onAppear {
-                        selectedTypes = selectedTherapies.compactMap { TherapyType(rawValue: $0.therapyType!) }
-                    }
-                    .padding()
                 }
+                
+                Spacer()
+                Button("Done") {
+                    // Check for fewer than 2 types
+                    if selectedTypes.count < 2 {
+                        alertTitle = "Too Few Types"
+                        alertMessage = "Please select at least two types."
+                        showAlert = true
+                    } else {
+                        // Save the selected types and dismiss the view.
+                        saveSelectedTherapies(therapyTypes: selectedTypes, context: managedObjectContext)
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
             }
-
+            .onAppear {
+                selectedTypes = selectedTherapies.compactMap { TherapyType(rawValue: $0.therapyType!) }
+            }
+            .padding()
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+        }
+    }
+    
     // Saves a therapy type to Core Data.
     func saveTherapyType(type: TherapyType, context: NSManagedObjectContext) {
         let selectedTherapy = SelectedTherapy(context: context)
@@ -82,11 +100,11 @@ struct TherapyTypeSelectionView: View {
             print("Failed to save therapy type: \(error)")
         }
     }
-
+    
     // Deletes all selected therapies from Core Data.
     func deleteAllTherapies(context: NSManagedObjectContext) {
         let fetchRequest: NSFetchRequest<NSFetchRequestResult> = SelectedTherapy.fetchRequest()
-
+        
         do {
             if let results = try context.fetch(fetchRequest) as? [NSManagedObject] {
                 for object in results {
@@ -98,13 +116,13 @@ struct TherapyTypeSelectionView: View {
             print("Failed to delete therapies: \(error)")
         }
     }
-
-
+    
+    
     // Saves the selected therapies, deleting any existing selections first.
     func saveSelectedTherapies(therapyTypes: [TherapyType], context: NSManagedObjectContext) {
         // Delete all existing selections.
         deleteAllTherapies(context: context)
-
+        
         // Save new selections.
         for type in therapyTypes {
             saveTherapyType(type: type, context: context)
