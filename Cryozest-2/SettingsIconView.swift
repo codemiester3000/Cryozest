a4822ed3fa653af2454ae050eb1b6a73158c5cf2
codemiester3000 <owenkamilkhoury@gmail.com>
2023-06-25 22:52:31 -0400

    new animation for gear icon

--- Cryozest-2/SettingsIconView.swift
+++ Cryozest-2/SettingsIconView.swift
@@ -5,7 +5,7 @@ class SettingsIconViewModel: ObservableObject {
     private var timer: Timer?
 
     init() {
-        timer = Timer.scheduledTimer(withTimeInterval: 10.0, repeats: true) { [weak self] _ in
+        timer = Timer.scheduledTimer(withTimeInterval: 8.0, repeats: true) { [weak self] _ in
             withAnimation {
                 self?.rotationDegrees += 360
             }
@@ -15,14 +15,27 @@ class SettingsIconViewModel: ObservableObject {
 
 struct SettingsIconView: View {
     @ObservedObject var viewModel = SettingsIconViewModel()
+    private var colors: [Color] = [.orange, .blue, .green]
+    @State private var currentColorIndex = 0
 
     var body: some View {
         Image(systemName: "gearshape.fill")
-            .foregroundColor(.orange)
+            .foregroundColor(colors[currentColorIndex])
             .font(.title)
             .rotationEffect(.degrees(viewModel.rotationDegrees))
             //.offset(y: 20)
             .animation(.easeInOut(duration: 1), value: viewModel.rotationDegrees)
+            .onReceive(viewModel.$rotationDegrees) { _ in
+                updateColor()
+            }
+    }
+
+    private func updateColor() {
+        let newIndex = (currentColorIndex + 1) % colors.count
+        withAnimation {
+            currentColorIndex = newIndex
+        }
     }
 }
+
 
