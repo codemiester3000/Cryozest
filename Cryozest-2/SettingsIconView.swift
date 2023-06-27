03696a592d26b0f6b79d8263817ef4a5685aeb3f
codemiester3000 <owenkamilkhoury@gmail.com>
2023-06-26 23:25:41 -0400

    ui tweak

--- Cryozest-2/SettingsIconView.swift
+++ Cryozest-2/SettingsIconView.swift
@@ -19,15 +19,29 @@ struct SettingsIconView: View {
     @State private var currentColorIndex = 0
 
     var body: some View {
-        Image(systemName: "gearshape.fill")
-            .foregroundColor(colors[currentColorIndex])
-            .font(.title)
-            .rotationEffect(.degrees(viewModel.rotationDegrees))
-            //.offset(y: 20)
-            .animation(.easeInOut(duration: 1), value: viewModel.rotationDegrees)
-            .onReceive(viewModel.$rotationDegrees) { _ in
-                updateColor()
+        HStack {
+            Spacer()
+            ZStack {
+                Circle()
+                    .fill(Color.gray.opacity(0.2))
+                    .frame(width: 40, height: 32) // Reduced height for the pill
+                Image(systemName: "gearshape.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(colors[currentColorIndex])
+                    .frame(width: 30, height: 30)
+                    .rotationEffect(.degrees(viewModel.rotationDegrees))
             }
+            Spacer()
+        }
+        //.padding()
+        //.background(Color.white) // Gray, slightly transparent background
+        .cornerRadius(20)
+        .shadow(color: Color.black.opacity(0.2), radius: 4, x: 0, y: 2)
+        .animation(.easeInOut(duration: 1), value: viewModel.rotationDegrees)
+//        .onReceive(viewModel.$rotationDegrees) { _ in
+//            updateColor()
+//        }
     }
 
     private func updateColor() {
@@ -37,5 +51,3 @@ struct SettingsIconView: View {
         }
     }
 }
-
-
