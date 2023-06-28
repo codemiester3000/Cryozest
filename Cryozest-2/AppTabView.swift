2d2568a708a9ca9a3d27c3fe89a81ff86ab81cc2
codemiester3000 <owenkamilkhoury@gmail.com>
2023-06-27 21:49:13 -0400

    Put 0.85 opacity over entire screen

--- Cryozest-2/AppTabView.swift
+++ Cryozest-2/AppTabView.swift
@@ -29,7 +29,7 @@ struct AppTabView: View {
                 .toolbarBackground(Color(red: 0.675, green: 0.675, blue: 0.675), for: .tabBar)
                 .toolbarBackground(.visible, for: .tabBar)
         }
-        .accentColor(Color.orange).opacity(0.9).saturation(0.7)
+        .accentColor(Color.orange).opacity(0.85)
         .background(Color.clear)
     }
 }
