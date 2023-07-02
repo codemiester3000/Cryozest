0e99b021dd5998fac2a355cb45a5f30211698be4
codemiester3000 <owenkamilkhoury@gmail.com>
2023-07-01 21:27:36 -0400

    improve UI of the timer selections

--- Cryozest-2/CreateTimerView.swift
+++ Cryozest-2/CreateTimerView.swift
@@ -11,21 +11,57 @@ struct CreateTimerView: View {
     @State private var durations = [5, 10, 15]
     
     var body: some View {
-        VStack {
-            ForEach(0..<3) { index in
-                Picker(selection: $durations[index], label: Text("Duration")) {
-                    ForEach(1...60, id: \.self) {
-                        Text("\($0) min")
+        ZStack {  // Wrapping content in ZStack for full background coverage
+            LinearGradient(
+                gradient: Gradient(colors: [Color.gray, Color.gray.opacity(0.8)]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .edgesIgnoringSafeArea(.all) // Now the gradient covers whole background
+            
+            VStack(spacing: 20) {
+                ForEach(0..<3) { index in
+                    VStack {  // Added VStack for each picker and title
+                        Text("Timer \(index + 1)") // Titles for each picker
+                            .font(.headline)
+                            .fontWeight(.medium)
+                            .foregroundColor(.white)
+                        Picker(selection: $durations[index], label: Text("Duration")) {
+                            ForEach(1...60, id: \.self) {
+                                Text("\($0) min")
+                            }
+                        }
+                        .pickerStyle(WheelPickerStyle())
+                        .background(Color.white.opacity(0.2))
+                        .cornerRadius(15)
+                        .frame(height: 100) // Reduced height for pickers
+                        .foregroundColor(.white)
+                        .labelsHidden()
                     }
+                    .padding(.top, 20)
                 }
+                
+                Spacer()
+                
+                Button(action: {
+                    saveDurations()
+                    self.presentationMode.wrappedValue.dismiss()
+                }) {
+                    Text("Done")
+                        .font(.system(size: 28, weight: .bold, design: .monospaced))
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 80)
+                        .padding(.vertical, 14)
+                        .background(.orange
+                        )
+                        .cornerRadius(40)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
+                }
+                .padding()
             }
-            Button(action: {
-                saveDurations()
-                self.presentationMode.wrappedValue.dismiss() // Add this line
-            }) {
-                Text("Done")
-            }
-        }.onAppear(perform: loadTimers)
+            .padding()
+        }
+        .onAppear(perform: loadTimers)
     }
     
     private func loadTimers() {
