7ec89a76d4312825069420380dbed91d9540cccc
codemiester3000 <owenkamilkhoury@gmail.com>
2023-07-02 20:12:19 -0400

    initial attempt at refactoring the mainview

--- Cryozest-2/MainView.swift
+++ Cryozest-2/MainView.swift
@@ -2,9 +2,136 @@ import SwiftUI
 import HealthKit
 import CoreData
 
+class MainViewModel: ObservableObject {
+    @Published var therapyTypeSelection: TherapyTypeSelection
+    @Published var timerLabel: String = "00:00"
+    @Published var timerDuration: TimeInterval = 0
+    @Published var timerStartDate: Date?
+    @Published var showAlert: Bool = false
+    @Published var alertTitle: String = ""
+    @Published var alertMessage: String = ""
+    @Published var showLogbook: Bool = false
+    @Published var showSessionSummary: Bool = false
+    @Published var therapyType: TherapyType = .drySauna
+    @Published var isRunning = false
+    @Published var averageHeartRate: Double = 0.0
+    @Published var minHeartRate: Double = 1000.0
+    @Published var maxHeartRate: Double = 0.0
+    @Published var averageSpo2: Double = 0.0
+    @Published var averageRespirationRate: Double = 0.0
+    @Published var isHealthDataAvailable: Bool = false
+    @Published var acceptedHealthKitPermissions: Bool = false
+    @Published var countDown: Bool = false
+    @Published var initialTimerDuration: TimeInterval = 0
+    @Published var showCreateTimer = false
+    
+    var healthStore = HKHealthStore()
+    let sleepAnalysisType = HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!
+    let heartRateType = HKObjectType.quantityType(forIdentifier: .heartRate)!
+    let respirationRateType = HKObjectType.quantityType(forIdentifier: .respiratoryRate)!
+    let spo2Type = HKObjectType.quantityType(forIdentifier: .oxygenSaturation)!
+    
+    var timer: Timer?
+    var healthDataTimer: Timer?
+    
+    init(therapyTypeSelection: TherapyTypeSelection) {
+        self.therapyTypeSelection = therapyTypeSelection
+    }
+    
+    func startCountdown(for seconds: TimeInterval) {
+        initialTimerDuration = seconds
+        timerDuration = seconds
+        countDown = true
+        startStopButtonPressed()
+    }
+    
+    func startStopButtonPressed() {
+        // Timer has not started (shows 'start').
+        if timer == nil {
+            HealthKitManager.shared.requestAuthorization { success, error in
+                DispatchQueue.main.async {
+                    if success {
+                        // pullHealthData()
+                        self.acceptedHealthKitPermissions = true
+                    } else {
+                        self.acceptedHealthKitPermissions = false
+                        // showAlert(title: "Authorization Failed", message: "Failed to authorize HealthKit access.")
+                    }
+                }
+            }
+            
+            timerStartDate = Date()
+            pullHealthData()
+            
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                if self.countDown {
+                    self.timerDuration -= 1
+                    if self.timerDuration <= 0 {
+                        self.timer?.invalidate()
+                        self.timer = nil
+                        self.countDown = false
+                        self.showSummary()
+                        self.timerLabel = "00:00"
+                        return
+                    }
+                } else {
+                    self.timerDuration = Date().timeIntervalSince(self.timerStartDate!)
+                }
+                let minutes = Int(self.timerDuration) / 60
+                let seconds = Int(self.timerDuration) % 60
+                self.timerLabel = String(format: "%02d:%02d", minutes, seconds)
+            }
+        } else { // Timer is running (shows 'stop').
+            timer?.invalidate()
+            timer = nil
+            healthDataTimer?.invalidate()
+            healthDataTimer = nil
+            showSummary()
+            timerLabel = "00:00"
+            
+            // For stopwatch timers
+            if countDown {
+                timerDuration = initialTimerDuration - timerDuration
+                countDown = false
+            }
+        }
+    }
+    
+    func pullHealthData() {
+        healthDataTimer = Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { _ in
+            let startDate = self.timerStartDate!
+            let endDate = Date()
+            
+            HealthKitManager.shared.fetchHealthData(from: startDate, to: endDate) { healthData in
+                if let healthData = healthData {
+                    self.averageHeartRate = healthData.avgHeartRate  // This line was changed
+                    self.averageSpo2 = healthData.avgSpo2
+                    self.averageRespirationRate = healthData.avgRespirationRate
+                    self.minHeartRate = healthData.minHeartRate
+                    self.maxHeartRate = healthData.maxHeartRate
+                }
+            }
+        }
+    }
+    
+    func showSummary() {
+        withAnimation {
+            showSessionSummary = true
+        }
+    }
+    
+    func showAlert(title: String, message: String) {
+        alertTitle = title
+        alertMessage = message
+        showAlert = true
+    }
+}
+
 struct MainView: View {
     
+    @ObservedObject var viewModel: MainViewModel
     @ObservedObject var therapyTypeSelection: TherapyTypeSelection
+    
     
     @Environment(\.scenePhase) private var scenePhase
     
@@ -24,50 +151,21 @@ struct MainView: View {
     
     var selectedTherapyTypes: [TherapyType] {
         // Convert the selected therapy types from strings to TherapyType values
-        if selectedTherapies.isEmpty {
+        if selectedTherapies.isEmpty {  
             return [.drySauna, .weightTraining, .coldPlunge, .meditation]
         } else {
             return selectedTherapies.compactMap { TherapyType(rawValue: $0.therapyType ?? "") }
         }
     }
     
-    let healthStore = HKHealthStore()
-    let sleepAnalysisType = HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!
-    let heartRateType = HKObjectType.quantityType(forIdentifier: .heartRate)!
-    let respirationRateType = HKObjectType.quantityType(forIdentifier: .respiratoryRate)!
-    let spo2Type = HKObjectType.quantityType(forIdentifier: .oxygenSaturation)!
-    let gridItems = [GridItem(.flexible()), GridItem(.flexible())]
-    
     @FetchRequest(
         entity: TherapySessionEntity.entity(),
         sortDescriptors: [])
     private var sessions: FetchedResults<TherapySessionEntity>
     
-    @State private var timerLabel: String = "00:00"
-    @State private var timer: Timer?
-    @State private var healthDataTimer: Timer?
-    @State private var timerDuration: TimeInterval = 0
-    @State private var timerStartDate: Date?
-    @State private var showAlert: Bool = false
-    @State private var alertTitle: String = ""
-    @State private var alertMessage: String = ""
-    @State private var showLogbook: Bool = false
-    @State private var showSessionSummary: Bool = false
-    @State private var therapyType: TherapyType = .drySauna
-    @State private var isRunning = false
-    @State private var averageHeartRate: Double = 0.0
-    @State private var minHeartRate: Double = 1000.0
-    @State private var maxHeartRate: Double = 0.0
-    @State private var averageSpo2: Double = 0.0
-    @State private var averageRespirationRate: Double = 0.0
-    @State private var isHealthDataAvailable: Bool = false
-    @State private var acceptedHealthKitPermissions: Bool = false
-    @State private var countDown: Bool = false
-    @State private var initialTimerDuration: TimeInterval = 0
-    @State private var showCreateTimer = false
-    
     init(therapyTypeSelection: TherapyTypeSelection) {
         self.therapyTypeSelection = therapyTypeSelection
+        self.viewModel = MainViewModel(therapyTypeSelection: therapyTypeSelection)
     }
     
     var body: some View {
@@ -81,7 +179,7 @@ struct MainView: View {
                 TherapyTypeGrid(therapyTypeSelection: therapyTypeSelection, selectedTherapyTypes: selectedTherapyTypes)
                     .padding(.bottom, 18)
                 
-                Text(timerLabel)
+                Text(viewModel.timerLabel)
                     .font(.system(size: 72, weight: .bold, design: .monospaced))
                     .foregroundColor(.white)
                     .padding(EdgeInsets(top: 18, leading: 36, bottom: 18, trailing: 36))
@@ -93,7 +191,7 @@ struct MainView: View {
                 HStack(spacing: 10) {
                     ForEach(customTimers, id: \.self) { timer in
                         Button(action: {
-                            startCountdown(for: Double(timer.duration) * 60)
+                            viewModel.startCountdown(for: Double(timer.duration) * 60)
                         }) {
                             Text("\(timer.duration) min")
                                 .font(.system(size: 16, weight: .bold, design: .monospaced))
@@ -104,12 +202,12 @@ struct MainView: View {
                                 .cornerRadius(40)
                                 .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
                         }
-                        .disabled(self.timer != nil)
-                        .opacity(self.timer != nil ? 0.3 : 1)
+                        .disabled(viewModel.timer != nil)
+                        .opacity(viewModel.timer != nil ? 0.3 : 1)
                     }
                     Button(action: {
                         // Navigate to a view for creating a new custom timer
-                        showCreateTimer = true
+                        viewModel.showCreateTimer = true
                     }) {
                         Image(systemName: "plus")
                             .font(.system(size: 16, weight: .bold, design: .monospaced))
@@ -120,14 +218,14 @@ struct MainView: View {
                             .cornerRadius(40)
                             .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
                     }
-                    .disabled(self.timer != nil)
-                    .opacity(self.timer != nil ? 0.3 : 1)
+                    .disabled(viewModel.timer != nil)
+                    .opacity(viewModel.timer != nil ? 0.3 : 1)
                     
                 }
                 .padding(.bottom, 28)
                 
-                Button(action: startStopButtonPressed) {
-                    Text(timer == nil ? "Start" : "Stop")
+                Button(action: viewModel.startStopButtonPressed) {
+                    Text(viewModel.timer == nil ? "Start" : "Stop")
                         .font(.system(size: 28, weight: .bold, design: .monospaced))
                         .foregroundColor(.white)
                         .padding(.horizontal, 80)
@@ -140,27 +238,27 @@ struct MainView: View {
                 
                 Spacer()
                 
-                Text(isHealthDataAvailable ? "Health data from sessions is available only with an Apple Watch" : "Enable HealthKit permissions for Cyrozest to give you the full health tracking experience. Visit Settings --> Privacy --> Health to grant access.")
+                Text(viewModel.isHealthDataAvailable ? "Health data from sessions is available only with an Apple Watch" : "Enable HealthKit permissions for Cyrozest to give you the full health tracking experience. Visit Settings --> Privacy --> Health to grant access.")
                     .foregroundColor(.white)
                     .font(.system(size: 12))
                     .multilineTextAlignment(.center)
                     .padding(.horizontal, 20)
                     .padding(.bottom, 26)
                 
-                NavigationLink(destination: LogbookView(therapyTypeSelection: self.therapyTypeSelection), isActive: $showLogbook) {
+                NavigationLink(destination: LogbookView(therapyTypeSelection: self.therapyTypeSelection), isActive: $viewModel.showLogbook) {
                     EmptyView()
                 }
                 NavigationLink(
                     destination: SessionSummary(
-                        duration: timerDuration,
-                        therapyType: $therapyType,
-                        averageHeartRate: averageHeartRate,
-                        averageSpo2: averageSpo2,
-                        averageRespirationRate: averageRespirationRate,
-                        minHeartRate: minHeartRate,
-                        maxHeartRate: maxHeartRate
+                        duration: viewModel.timerDuration,
+                        therapyType: $viewModel.therapyType,
+                        averageHeartRate: viewModel.averageHeartRate,
+                        averageSpo2: viewModel.averageSpo2,
+                        averageRespirationRate: viewModel.averageRespirationRate,
+                        minHeartRate: viewModel.minHeartRate,
+                        maxHeartRate: viewModel.maxHeartRate
                     ),
-                    isActive: $showSessionSummary
+                    isActive: $viewModel.showSessionSummary
                 ) {
                     EmptyView()
                 }
@@ -193,20 +291,20 @@ struct MainView: View {
                 
                 // On first load always have the first therapyType selected.
                 if let firstTherapy = selectedTherapyTypes.first {
-                    therapyType = firstTherapy
+                    viewModel.therapyType = firstTherapy
                 }
                 
                 HealthKitManager.shared.requestAuthorization { success, error in
                     if success {
                         HealthKitManager.shared.areHealthMetricsAuthorized() { isAuthorized in
-                            isHealthDataAvailable = isAuthorized
+                            viewModel.isHealthDataAvailable = isAuthorized
                         }
                     } else {
-                        showAlert(title: "Authorization Failed", message: "Failed to authorize HealthKit access.")
+                        // showAlert(title: "Authorization Failed", message: "Failed to authorize HealthKit access.")
                     }
                 }
             }
-            .sheet(isPresented: $showCreateTimer) {
+            .sheet(isPresented: $viewModel.showCreateTimer) {
                 CreateTimerView()
                     .environment(\.managedObjectContext, self.viewContext)
             }
@@ -226,18 +324,18 @@ struct MainView: View {
                 
                 if remainingTime > 0 {
                     // Timer was running when app went to background
-                    self.timerStartDate = timerStartDate
-                    self.timerDuration = remainingTime
+                    viewModel.timerStartDate = timerStartDate
+                    viewModel.timerDuration = remainingTime
                 } else {
                     // Timer was not running
-                    self.timerStartDate = nil
-                    self.timerDuration = 0
+                    viewModel.timerStartDate = nil
+                    viewModel.timerDuration = 0
                 }
                 
             case .inactive, .background:
                 // App has gone to the background, save timer state
-                UserDefaults.standard.set(self.timerStartDate, forKey: "timerStartDate")
-                UserDefaults.standard.set(self.initialTimerDuration, forKey: "timerDuration")
+                UserDefaults.standard.set(viewModel.timerStartDate, forKey: "timerStartDate")
+                UserDefaults.standard.set(viewModel.initialTimerDuration, forKey: "timerDuration")
                 
             @unknown default:
                 // Future cases
@@ -245,101 +343,4 @@ struct MainView: View {
             }
         }
     }
-    
-    func startCountdown(for seconds: TimeInterval) {
-        initialTimerDuration = seconds
-        timerDuration = seconds
-        countDown = true
-        startStopButtonPressed()
-    }
-    
-    func startStopButtonPressed() {
-        // Timer has not started (shows 'start').
-        if timer == nil {
-            HealthKitManager.shared.requestAuthorization { success, error in
-                DispatchQueue.main.async {
-                    if success {
-                        // pullHealthData()
-                        self.acceptedHealthKitPermissions = true
-                    } else {
-                        self.acceptedHealthKitPermissions = false
-                        showAlert(title: "Authorization Failed", message: "Failed to authorize HealthKit access.")
-                    }
-                }
-            }
-            
-            timerStartDate = Date()
-            pullHealthData()
-            
-            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-                
-                if self.countDown {
-                    timerDuration -= 1
-                    if timerDuration <= 0 {
-                        timer?.invalidate()
-                        timer = nil
-                        countDown = false
-                        showSummary()
-                        timerLabel = "00:00"
-                        return
-                    }
-                } else {
-                    timerDuration = Date().timeIntervalSince(timerStartDate!)
-                }
-                
-                // timerDuration = Date().timeIntervalSince(timerStartDate!)
-                let minutes = Int(timerDuration) / 60
-                let seconds = Int(timerDuration) % 60
-                timerLabel = String(format: "%02d:%02d", minutes, seconds)
-            }
-        } else { // Timer is running (shows 'stop').
-            timer?.invalidate()
-            timer = nil
-            healthDataTimer?.invalidate()
-            healthDataTimer = nil
-            showSummary()
-            timerLabel = "00:00"
-            
-            // For stopwatch timers
-            if countDown {
-                timerDuration = initialTimerDuration - timerDuration
-                countDown = false
-            }
-        }
-    }
-    
-    func pullHealthData() {
-        healthDataTimer = Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { _ in
-            let startDate = timerStartDate!
-            let endDate = Date()
-            
-            HealthKitManager.shared.fetchHealthData(from: startDate, to: endDate) { healthData in
-                if let healthData = healthData {
-                    averageHeartRate = healthData.avgHeartRate  // This line was changed
-                    averageSpo2 = healthData.avgSpo2
-                    averageRespirationRate = healthData.avgRespirationRate
-                    minHeartRate = healthData.minHeartRate
-                    maxHeartRate = healthData.maxHeartRate
-                }
-            }
-        }
-    }
-    
-    func showSummary() {
-        withAnimation {
-            showSessionSummary = true
-        }
-    }
-    
-    func showAlert(title: String, message: String) {
-        alertTitle = title
-        alertMessage = message
-        showAlert = true
-    }
 }
-
-extension Color {
-    static let darkBackground = Color(red: 26 / 255, green: 32 / 255, blue: 44 / 255)
-    static let customBlue = Color(red: 30 / 255, green: 144 / 255, blue: 255 / 255)
-}
-
