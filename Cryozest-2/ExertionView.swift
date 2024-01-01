7c9164fbf2440792a44a75b9dd50549c5e7837ba
robertamarin <127469693+robertamarin@users.noreply.github.com>
2023-12-31 18:22:31 -0800

    Added in HR Zone graphs

--- Cryozest-2/ExertionView.swift
+++ Cryozest-2/ExertionView.swift
@@ -5,6 +5,8 @@ class ExertionModel: ObservableObject {
     let healthStore = HKHealthStore()
     
     @Published var exertionScore: Double = 0.0
+    @Published var zoneTimes: [Double] = []
+    
     
     init() {
         fetchExertionScore()
@@ -14,11 +16,11 @@ class ExertionModel: ObservableObject {
         // Fetch the user's age from HealthKit or default to 30 if unavailable
         HealthKitManager.shared.fetchUserAge { [weak self] (age: Int?, error: Error?) in
             let userAge = age ?? 30 // Use the fetched age or default to 30
-
+            
             // Set startDate to the beginning of the current day
             let startDate = Calendar.current.startOfDay(for: Date())
             let endDate = Date()
-
+            
             HealthKitManager.shared.fetchHeartRateData(from: startDate, to: endDate) { (results, error) in
                 // ... existing implementation ...
                 if let error = error {
@@ -26,7 +28,7 @@ class ExertionModel: ObservableObject {
                     return
                 }
                 guard let results = results else { return }
-
+                
                 DispatchQueue.global().async {
                     do {
                         let score = try self?.calculateExertionScore(userAge: userAge, heartRateData: results)
@@ -40,25 +42,32 @@ class ExertionModel: ObservableObject {
             }
         }
     }
-
+    
     
     private func calculateExertionScore(userAge: Int, heartRateData: [HKQuantitySample]) throws -> Double {
         let zoneMultipliers: [Double] = [0.0668, 0.1198, 0.13175, 0.1581, 0.18975]
-        let zoneUpperBoundaries: [Double] = [0.6, 0.7, 0.8, 0.9, 1.0]
+        let zoneUpperBoundaries: [Double] = [0.6, 0.7, 0.8, 0.9, 1.1]  // Assuming these are the correct percentages of max HR
         
         var exertionScore = 0.0
-
+        
+        // Reset zoneTimes on the main thread
+        DispatchQueue.main.async {
+            self.zoneTimes = []
+        }
+        
         for (index, upperBoundary) in zoneUpperBoundaries.enumerated() {
             let lowerBoundary = index == 0 ? 0.5 : zoneUpperBoundaries[index - 1]
             let timeInZone = calculateTimeInZone(for: heartRateData, userAge: userAge, lowerBoundMultiplier: lowerBoundary, upperBoundMultiplier: upperBoundary)
             exertionScore += timeInZone * zoneMultipliers[index]
+            
+            // Append to zoneTimes on the main thread
+            DispatchQueue.main.async {
+                self.zoneTimes.append(timeInZone)
+            }
         }
         
         return exertionScore
     }
-
-
-    
     
     private func calculateTimeInZone(for samples: [HKQuantitySample], userAge: Int, lowerBoundMultiplier: Double, upperBoundMultiplier: Double) -> Double {
         var zoneTime: TimeInterval = 0
@@ -84,7 +93,10 @@ class ExertionModel: ObservableObject {
         }
         
         let minutesInZone = zoneTime / 60
-        print("Time in Zone [\(lowerBoundHeartRate)-\(upperBoundHeartRate)]: \(minutesInZone) minutes")
+        // Make sure to print on the main thread if you're updating the UI
+        DispatchQueue.main.async {
+            print("Time in Zone [\(lowerBoundHeartRate)-\(upperBoundHeartRate)]: \(minutesInZone) minutes")
+        }
         return minutesInZone
     }
 }
@@ -97,18 +109,59 @@ func clamp(_ value: Double, to range: ClosedRange<Double>) -> Double {
 
 struct ExertionView: View {
     @ObservedObject var model: ExertionModel
- 
-    var body: some View {
-        VStack {
-            Text("Exertion Score")
-                .font(.headline)
-                .padding()
 
-            ExertionRingView(exertionScore: model.exertionScore)
-                .frame(width: 120, height: 120)
+    var body: some View {
+        ScrollView {
+            VStack {
+                HStack {
+                    Text("Daily Exertion")
+                        .font(.title2) // Adjusted font size
+                        .fontWeight(.semibold) // Adjusted font weight
+                        .foregroundColor(.white)
+                        .padding(.leading)
+
+                    Spacer()
+
+                    ExertionRingView(exertionScore: model.exertionScore)
+                        .frame(width: 120, height: 120)
+                }
+                .padding(.vertical, 20)
+
+                // Dynamically create zoneInfos from model.zoneTimes
+                let maxTime = model.zoneTimes.max() ?? 1
+                let zoneInfos = model.zoneTimes.enumerated().map { (index, timeInMinutes) -> ZoneInfo in
+                    // Define your color array matching the zones
+                    let colors: [Color] = [.blue, .cyan, .green, .orange, .pink]
+                    let timeSpentString = formatTime(timeInMinutes: timeInMinutes)
+                    
+                    return ZoneInfo(
+                        zoneNumber: index + 1,
+                        timeSpent: timeSpentString,
+                        color: colors[index % colors.count],
+                        timeInMinutes: timeInMinutes
+                    )
+                }
+
+                ForEach(zoneInfos, id: \.zoneNumber) { zoneInfo in
+                    ZoneItemView(zoneInfo: zoneInfo, maxTime: maxTime)
+                }
+            }
+            .padding(.horizontal)
         }
+        .background(Color.black.edgesIgnoringSafeArea(.all))
+    }
+    
+    // Helper function to format the time from minutes to a string
+    func formatTime(timeInMinutes: Double) -> String {
+        let totalSeconds = Int(timeInMinutes * 60)
+        let minutes = totalSeconds / 60
+        let seconds = totalSeconds % 60
+        return String(format: "%02d:%02d", minutes, seconds)
     }
 }
+
+
+
 
 struct ExertionRingView: View {
     var exertionScore: Double
@@ -139,15 +192,53 @@ struct ExertionRingView: View {
           }
       }
 
+struct ZoneInfo {
+    var zoneNumber: Int
+    var timeSpent: String
+    var color: Color
+    var timeInMinutes: Double  // Add this property to store the time in minutes
+}
 
 
+struct ZoneItemView: View {
+    var zoneInfo: ZoneInfo
+    var maxTime: Double
 
+    var body: some View {
+        HStack {
+            Circle()
+                .fill(zoneInfo.color)
+                .frame(width: 10, height: 10)
+            Text("Zone \(zoneInfo.zoneNumber)")
+                .foregroundColor(.white)
+                .padding(.leading, 5)
+            
+            Spacer()
 
+            // Foreground of the progress bar
+            GeometryReader { geometry in
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .frame(width: geometry.size.width, height: 5)
+                        .foregroundColor(Color.gray.opacity(0.3))
+                        .cornerRadius(2.5)
 
+                    if zoneInfo.timeInMinutes > 0 {
+                        Rectangle()
+                            .frame(width: geometry.size.width * CGFloat(zoneInfo.timeInMinutes / maxTime), height: 5)
+                            .foregroundColor(zoneInfo.color)
+                            .cornerRadius(2.5)
+                    }
+                }
+            }
+            .frame(height: 5)
 
-
-
-
-
-
-
+            Text(zoneInfo.timeSpent)
+                .foregroundColor(.white)
+                .padding(.leading, 5)
+        }
+        .padding()
+        .background(Color.black.opacity(0.8))
+        .cornerRadius(5)
+    }
+}
