df8bccb2726959263071c8e79f163486d529f580
robertamarin <127469693+robertamarin@users.noreply.github.com>
2024-01-02 09:59:41 -0800

    Exertion Target added still not pulling in correct recovery

--- Cryozest-2/ExertionView.swift
+++ Cryozest-2/ExertionView.swift
@@ -111,16 +111,14 @@ struct ExertionView: View {
     var body: some View {
         VStack {
             HStack {
-                // VStack for the title and the target exertion zone text
                 VStack(alignment: .leading) {
                     HStack {
                         Text("Daily Exertion")
                             .font(.title2)
                             .fontWeight(.semibold)
                             .foregroundColor(.white)
-
+                        
                         Button(action: {
-                            // Toggle the visibility of the popover
                             isPopoverVisible.toggle()
                         }) {
                             ZStack {
@@ -132,22 +130,25 @@ struct ExertionView: View {
                                     .font(.headline)
                             }
                         }
-<<<<<<< HEAD
                         .padding(.vertical, 20)
-                
-=======
-                        .padding(.leading, 5) // Control space between the title and the button
+                        .padding(.leading, 5)
                     }
-
-                    // Target Exertion Zone text below the title
+                    
                     Text("Today's Exertion Target: \(targetExertionZone)")
                         .font(.headline)
                         .foregroundColor(.gray)
+                    
+                    // Debugging Text View
+                    //                    Text("Debug Recovery Score: \(recoveryModel.recoveryScore ?? -1)")
+                    //                        .foregroundColor(.white)
+                    //                        .onAppear {
+                    //                            print("Debug Recovery Score Appeared: \(recoveryModel.recoveryScore ?? -1)")
+                    //                        }
                 }
                 .padding(.leading)
-
-                Spacer() // Pushes everything to the left and the ExertionRingView to the right
-
+                
+                Spacer()
+                
                 ExertionRingView(exertionScore: model.exertionScore)
                     .frame(width: 120, height: 120)
                     .padding(.trailing, 20)
@@ -156,77 +157,76 @@ struct ExertionView: View {
         }
         
         
->>>>>>> e6fcd63 (Exertion Target added)
-                // Dynamically create zoneInfos from model.zoneTimes
-                let maxTime = model.zoneTimes.max() ?? 1
-                let zoneInfos = model.zoneTimes.enumerated().map { (index, timeInMinutes) -> ZoneInfo in
-                    // Define your color array matching the zones
-                    let colors: [Color] = [.blue, .cyan, .green, .orange, .pink]
-                    let timeSpentString = formatTime(timeInMinutes: timeInMinutes)
-                    
-                    return ZoneInfo(
-                        zoneNumber: index + 1,
-                        timeSpent: timeSpentString,
-                        color: colors[index % colors.count],
-                        timeInMinutes: timeInMinutes
-                    )
-                }
-                
-                ForEach(zoneInfos, id: \.zoneNumber) { zoneInfo in
-                    ZoneItemView(zoneInfo: zoneInfo, maxTime: maxTime)
-                }
+        // Dynamically create zoneInfos from model.zoneTimes
+        let maxTime = model.zoneTimes.max() ?? 1
+        let zoneInfos = model.zoneTimes.enumerated().map { (index, timeInMinutes) -> ZoneInfo in
+            // Define your color array matching the zones
+            let colors: [Color] = [.blue, .cyan, .green, .orange, .pink]
+            let timeSpentString = formatTime(timeInMinutes: timeInMinutes)
             
-
+            return ZoneInfo(
+                zoneNumber: index + 1,
+                timeSpent: timeSpentString,
+                color: colors[index % colors.count],
+                timeInMinutes: timeInMinutes
+            )
+        }
+        
+        ForEach(zoneInfos, id: \.zoneNumber) { zoneInfo in
+            ZoneItemView(zoneInfo: zoneInfo, maxTime: maxTime)
+        }
+        
+        
         
         .background(Color.black.edgesIgnoringSafeArea(.all))
-}
+    }
     
     // Helper function to format the time from minutes to a string
-        func formatTime(timeInMinutes: Double) -> String {
-            let totalSeconds = Int(timeInMinutes * 60)
-            let minutes = totalSeconds / 60
-            let seconds = totalSeconds % 60
-            return String(format: "%02d:%02d", minutes, seconds)
-        }
-
-        // Computed property for target exertion zone
-        var targetExertionZone: String {
-            let recoveryScore = recoveryModel.recoveryScore ?? 0
-               print("Current Recovery Score: \(recoveryScore)") // Debugging
-            switch recoveryScore {
-                case 90...100:
-                    return "9.0-10.0"
-                case 80..<90:
-                    return "8.0-9.0"
-                case 70..<80:
-                    return "7.0-8.0"
-                case 60..<70:
-                    return "6.0-7.0"
-                case 50..<60:
-                    return "5.0-6.0"
-                case 40..<50:
-                    return "4.0-5.0"
-                case 30..<40:
-                    return "3.0-4.0"
-                case 20..<30:
-                    return "2.0-3.0"
-                case 10..<20:
-                    return "1.0-2.0"
-                case 0..<10:
-                    return "0.0-1.0"
-                default:
-                    return "Not available"
-            }
+    func formatTime(timeInMinutes: Double) -> String {
+        let totalSeconds = Int(timeInMinutes * 60)
+        let minutes = totalSeconds / 60
+        let seconds = totalSeconds % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+    
+    // Computed property for target exertion zone
+    var targetExertionZone: String {
+        let recoveryScore = recoveryModel.recoveryScore ?? 0
+        print("Current Recovery Score: \(recoveryScore)") // Debugging
+        switch recoveryScore {
+        case 90...100:
+            return "9.0-10.0"
+        case 80..<90:
+            return "8.0-9.0"
+        case 70..<80:
+            return "7.0-8.0"
+        case 60..<70:
+            return "6.0-7.0"
+        case 50..<60:
+            return "5.0-6.0"
+        case 40..<50:
+            return "4.0-5.0"
+        case 30..<40:
+            return "3.0-4.0"
+        case 20..<30:
+            return "2.0-3.0"
+        case 10..<20:
+            return "1.0-2.0"
+        case 0..<10:
+            return "0.0-1.0"
+        default:
+            return "Not available"
         }
     }
+}
 
 
 func formatTime(timeInMinutes: Double) -> String {
-       let totalSeconds = Int(timeInMinutes * 60)
-       let minutes = totalSeconds / 60
-       let seconds = totalSeconds % 60
-       return String(format: "%02d:%02d", minutes, seconds)
-   }
+    let totalSeconds = Int(timeInMinutes * 60)
+    let minutes = totalSeconds / 60
+    let seconds = totalSeconds % 60
+    return String(format: "%02d:%02d", minutes, seconds)
+}
 
 
 
@@ -315,11 +315,11 @@ struct ExertionInfoPopoverView: View {
             Text("Exertion serves as a valuable indicator of your daily cardiovascular fitness load over the course of the day. This rating, measured on a scale of 0-10, is derived from your heart rate zones, where specific zones (such as Zone 1, Zone 2, Zone 3) correspond to different workout intensities. Exertion scores increase with higher workout intensity levels. To determine your maximum heart rate, a simple calculation (220 minus your age) is used to define these heart rate zones.")
                 .font(.system(size: 18))  // Adjust the font size here
                 .padding()
-
+            
             Text("The concept of exertion revolves around the duration spent exercising above your heart rate reserve, with higher scores allocated to more intense efforts. Your recommended exertion target zone depends on your current state of recovery and is presented as a suggestion. Always maintain a vigilant awareness of how your body responds. It's often wiser to stay below the recommended exertion target to prevent overtraining and protect your recovery process. Stay connected to your body, adjusting your training regimen based on your sensations and overall well-being.")
                 .font(.system(size: 18))  // Adjust the font size here
                 .padding()
-
+            
             Spacer()
         }
         .padding()
