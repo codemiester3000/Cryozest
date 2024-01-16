d5c06f31f3261f37b729f49f9692f90a0d0637d1
robertamarin <127469693+robertamarin@users.noreply.github.com>
2024-01-15 23:33:53 -0800

    Graphs with variance statements working

--- Cryozest-2/SleepVitalsGraph.swift
+++ Cryozest-2/SleepVitalsGraph.swift
@@ -65,11 +65,12 @@ class SleepVitalsDataModel: ObservableObject {
 
 struct SleepVitalsGraph: View {
     @ObservedObject var model: SleepVitalsDataModel
+    @Environment(\.managedObjectContext) var managedObjectContext
     
     var body: some View {
         VStack(alignment: .leading) {
             BarGraphView(
-                title: "Resting Heart Rate",
+                title: "Sleeping Resting Heart Rate",
                 baselineValue: model.baselineRestingHeartRate,
                 exerciseValue: model.exerciseRestingHeartRate,
                 baselineLabel: "\(model.baselineRestingHeartRate.isFinite ? Int(model.baselineRestingHeartRate) : 0) bpm",
@@ -79,7 +80,7 @@ struct SleepVitalsGraph: View {
             .padding(.bottom)
             
             BarGraphView(
-                title: "Resting Heart Rate Variability",
+                title: "Sleeping Heart Rate Variability",
                 baselineValue: model.baselineRestingHRV,
                 exerciseValue: model.exerciseRestingHRV,
                 baselineLabel: "\(model.baselineRestingHRV.isFinite ? Int(model.baselineRestingHRV) : 0) bpm",
@@ -88,8 +89,73 @@ struct SleepVitalsGraph: View {
             )
             .padding(.bottom)
             
+            
+            
+            ParagraphText("RHR",
+                          percentChange: calculatePercentChange(baseline: model.baselineRestingHeartRate,
+                                                                exercise: model.exerciseRestingHeartRate) ?? 0,
+                          therapyTypeDisplayName: model.therapyType.displayName(managedObjectContext))
+
+        
+                   ParagraphText("HRV",
+                                 percentChange: calculatePercentChange(baseline: model.baselineRestingHRV,
+                                                                       exercise: model.exerciseRestingHRV) ?? 0,
+                                 therapyTypeDisplayName: model.therapyType.displayName(managedObjectContext))
+             
         }
     }
+    
+    private func calculatePercentChange(baseline: Double, exercise: Double) -> CGFloat? {
+            if baseline != 0 {
+                return CGFloat((exercise - baseline) / baseline * 100)
+            }
+            return nil
+        }
+    
+    
+    @ViewBuilder
+    private func ParagraphText(_ metricType: String, percentChange: CGFloat, therapyTypeDisplayName: String) -> some View {
+        let indicator = changeIndicator(for: percentChange)
+        let percentChangeText = String(format: "%.1f", abs(percentChange))
+        let changeDescription = percentChange >= 0 ? "increase" : "decrease"
+        
+        HStack(spacing: 2) {
+            Text(indicator.symbol)
+                .font(.system(size: 12))
+                .foregroundColor(indicator.color)
+            
+            Text("You saw a ")
+                .font(.system(size: 12))
+                .foregroundColor(.white)
+            
+            + Text("\(percentChangeText)% ")
+                .font(.system(size: 12))
+                .foregroundColor(indicator.color)
+                .fontWeight(.bold)
+            
+            + Text("\(changeDescription) ")
+                .font(.system(size: 12))
+                .foregroundColor(indicator.color)
+                .fontWeight(.bold)
+            
+            + Text("in \(metricType) on ")
+                .font(.system(size: 12))
+                .foregroundColor(.white)
+            
+            + Text("\(therapyTypeDisplayName) days")
+                .font(.system(size: 12))
+                .foregroundColor(model.therapyType.color)
+        }
+        .padding(.bottom, 6)
+    }
+
+    private func changeIndicator(for percentChange: CGFloat) -> (symbol: String, color: Color) {
+        if percentChange == 0 {
+        return ("↑", .green)
+        } else {
+        return ("↓", .red)
+        }
+        }
 }
 
 struct BarGraphView: View {
@@ -100,50 +166,48 @@ struct BarGraphView: View {
     var exerciseLabel: String
     var barColor: Color
     
+    private let maxBarWidth: CGFloat = 200  // Maximum width of the bar
+    private let maxValue: Double = 100 // This should be your maximum scale value
+
+    
     @State private var baselineBarWidth: CGFloat = 0
     @State private var exerciseBarWidth: CGFloat = 0
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text(title)
-                .font(.footnote)
-                .foregroundColor(.white)
-            
-            // Baseline Bar with Label
-            HStack {
-                Rectangle()
-                    .fill(LinearGradient(gradient: Gradient(colors: [Color.gray.opacity(0.5), .gray]), startPoint: .leading, endPoint: .trailing))
-                    .frame(width: baselineBarWidth, height: 20)
-                    .cornerRadius(6.0)
-                
-                Text(baselineLabel)
-                    .font(.footnote)
-                    .foregroundColor(.white)
-            }
-            .onChange(of: baselineValue) { newValue in
-                withAnimation(.linear(duration: 3.0)) {
-                    baselineBarWidth = (baselineValue == 0 && exerciseValue == 0) ? 200 : newValue
-                }
-            }
-            
-            // Exercise Bar with Label
-            HStack {
-                Rectangle()
-                    .fill(LinearGradient(gradient: Gradient(colors: [barColor.opacity(0.6), barColor.opacity(0.9)]), startPoint: .leading, endPoint: .trailing))
-                    .frame(width: exerciseBarWidth, height: 20)
-                    .cornerRadius(6.0)
-                
-                Text(exerciseLabel)
-                    .font(.footnote)
-                    .foregroundColor(.white)
-            }
-            .onChange(of: exerciseValue) { newValue in
-                withAnimation(.linear(duration: 3.0)) {
-                    exerciseBarWidth = (baselineValue == 0 && exerciseValue == 0) ? 200 : newValue
-                }
-            }
-        }
-    }
-}
-
-
+          VStack(alignment: .leading) {
+              Text(title)
+                  .font(.footnote)
+                  .foregroundColor(.white)
+              
+              // Baseline Bar with Label
+              HStack {
+                  Rectangle()
+                      .fill(LinearGradient(gradient: Gradient(colors: [Color.gray.opacity(0.5), .gray]), startPoint: .leading, endPoint: .trailing))
+                      .frame(width: CGFloat(baselineValue / maxValue) * maxBarWidth, height: 20)
+                      .cornerRadius(6.0)
+                  
+                  Text(baselineLabel)
+                      .font(.footnote)
+                      .foregroundColor(.white)
+              }
+              
+              // Exercise Bar with Label
+              HStack {
+                  Rectangle()
+                      .fill(LinearGradient(gradient: Gradient(colors: [barColor.opacity(0.6), barColor.opacity(0.9)]), startPoint: .leading, endPoint: .trailing))
+                      .frame(width: CGFloat(exerciseValue / maxValue) * maxBarWidth, height: 20)
+                      .cornerRadius(6.0)
+                  
+                  Text(exerciseLabel)
+                      .font(.footnote)
+                      .foregroundColor(.white)
+              }
+          }
+          .onAppear {
+              // Trigger any necessary animations when the view appears
+              withAnimation(.linear(duration: 3.0)) {
+                  // Your animation code, if needed
+              }
+          }
+      }
+  }
