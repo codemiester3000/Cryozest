dfc814d9389e06ac054194592b68aefae0a89f29
codemiester3000 <owenkamilkhoury@gmail.com>
2024-01-21 13:46:28 -0500

    update animation time

--- Cryozest-2/SleepVitalsGraph.swift
+++ Cryozest-2/SleepVitalsGraph.swift
@@ -289,7 +289,7 @@ struct BarGraphView: View {
         .cornerRadius(15)
         .shadow(radius: 10)
         .onAppear {
-            withAnimation(.easeInOut(duration: 1.0)) {
+            withAnimation(.easeInOut(duration: 0.3)) {
                 baselineBarWidth = calculateBarWidth(value: baselineValue)
                 exerciseBarWidth = calculateBarWidth(value: exerciseValue)
             }
